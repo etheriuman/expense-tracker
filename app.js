87edf1b3ab51d704f59c35c222ddc648b9107ba9
etheriuman <timmy26291125@gmail.com>
2020-11-24 17:37:28 +0800

    fix heroku PORT var

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const routes = require('./routes')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // 連接資料庫
 require('./config/mongoose')
